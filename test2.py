1dc0644976cf2ae39074f9416753781908bdb79a
Terettetererere <92132913+Terettetererere@users.noreply.github.com>
2021-10-09 21:52:09 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -13,3 +13,39 @@ else:
 agree = st.checkbox('チェックしてください')
 if agree:
     st.write('Great!')
+
+fruit = st.radio(label="好きなフルーツは？",options =["バナナ","りんご","いちご"],index=1)
+st.write(fruit)
+
+option = st.selectbox('今日はなにする?', ('なわとび', '野球', 'ゲーム') )
+
+def f(i):
+    play = ('なわとび', '野球', 'ゲーム')
+    return play[i]
+
+option = st.selectbox('今日はなにする?', (0,1,2), format_func= f )
+st.write('よし ', option, " をしよう！")
+
+options = st.multiselect('何色が好き？',options= ['Green', 'Yellow', 'Red', 'Blue'], default=['Yellow', 'Red'])
+st.write('You selected:', options)
+
+age = st.slider('何歳?', min_value=0, max_value=130, value=25)
+st.write(age)
+
+from datetime import datetime, date, time
+interval = st.slider("計画期間は?", value=(datetime(2019, 1, 1, 9, 30),
+datetime(2020, 1, 1, 9, 30)),format="MM/DD/YY - hh:mm")
+
+color = st.select_slider('色を選んでね',options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+title = st.text_input('お名前は？', value='ななしのごんべえ')
+
+number = st.number_input('数字を入れてね', min_value=1.0, max_value=10.0, value=5.0, step=0.1)
+
+d = st.date_input("誕生日は？",date(2019, 7, 6))
+
+alarm = st.time_input('アラームセット', time(8, 45))
+
+uploaded_file = st.file_uploader("ファイル選択")
+
+add_selectbox = st.sidebar.selectbox("連絡方法は?", ("Email", "Home phone", "Mobile phone"))
