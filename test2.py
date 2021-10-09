ae9fac25a99bbfc86a1e41e94f1a2e4d8f22914d
Terettetererere <92132913+Terettetererere@users.noreply.github.com>
2021-10-09 21:47:06 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,15 +1,9 @@
 import streamlit as st
 import pandas as pd 
-import plotly.express as px
 
 st.title("タイトル")
 st.write("何でも書ける")
 st.markdown("## マークダウンで書く $x^2$")
-
-df = px.data.iris()
-st.write(df.head())
-st.table(df.head())
-st.write(px.scatter(df,x="sepal_length",y="sepal_width",color="species"))
 
 if st.button('Say hello'):
     st.write('こんにちわ')
